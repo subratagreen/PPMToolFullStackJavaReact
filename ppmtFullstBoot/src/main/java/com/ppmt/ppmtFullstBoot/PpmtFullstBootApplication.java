f9cc604774ed8819c5f7ec15ee5282054d9a07ed
subrat <subratagreen@gmail.com>
2020-03-22 09:47:10 +0530

    first commit

---
+++ ppmtFullstBoot/src/main/java/com/ppmt/ppmtFullstBoot/PpmtFullstBootApplication.java
@@ -0,0 +1,13 @@
+package com.ppmt.ppmtFullstBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtFullstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtFullstBootApplication.class, args);
+	}
+
+}
---
+++ ppmtFullstBoot/src/test/java/com/ppmt/ppmtFullstBoot/PpmtFullstBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppmt.ppmtFullstBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtFullstBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
